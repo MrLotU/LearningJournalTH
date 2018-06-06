476caab3d0caa56e89f357d2ee8082b5243c3be4
Jari Koopman <j.koopman@jarict.nl>
2018-06-06 14:10:44 +0200

    Update to required routes

--- app.py
+++ app.py
@@ -6,10 +6,11 @@ import wtforms
 app = Flask(__name__)
 
 @app.route('/')
+@app.route('/entries')
 def index():
     return render_template('index.html', entries=Entry.select())
 
-@app.route('/new', methods=['GET', 'POST'])
+@app.route('/entry', methods=['GET', 'POST'])
 def new():
     form = EntryForm(request.form)
     if request.method == 'POST' and form.validate():
@@ -17,18 +18,24 @@ def new():
         return redirect('/')
     return render_template('new.html', form=form)
 
-@app.route('/detail/<id>')
+@app.route('/entries/<id>')
 def detail(id):
     return render_template('detail.html', entry=Entry.get(Entry.id == id))
 
-@app.route('/edit/<id>', methods=['GET', 'POST'])
+@app.route('/entries/edit/<id>', methods=['GET', 'POST'])
 def edit(id):
     entry = Entry.get(Entry.id == id)
     form = EntryForm(request.form, entry)
-    if request.method == 'POTS' and form.validate():
-        Entry.update(**form.data).where(Entry.id == id)
+    if request.method == 'POST' and form.validate():
+        print(form.data)
+        Entry.update(**form.data).where(Entry.id == id).execute()
         return redirect('/')
     return render_template('edit.html', entry=entry, form=form)
+
+@app.route('/entries/delete/<id>')
+def delete(id):
+    Entry.delete().where(Entry.id == id).execute()
+    return redirect('/')
 
 if __name__ == '__main__':
     init_db()
